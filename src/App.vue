3bc5e0a48003560fc7fb26f2395f3dd1fb3ccaee
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-10-08 15:12:02 -0500

    created clickable link, added tailwindscss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
+import {ref} from 'vue';
 
 </script>
 
 <template>
-  <h1>Click Me for Fun</h1>
+  <h1>Chuck Norris "Facts"</h1>
+  <div>
+    <div>
+      <a class="text-bold text-emerald-700" href="https://api.chucknorris.io/jokes/random" target="\src\randomJoke.vue">Click Me for Fun!!!</a>
+
+    </div>
+  </div>
 </template>
